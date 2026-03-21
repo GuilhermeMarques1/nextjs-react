546b84224dc5fb89babd311386b4c4ae18aaf01b
GuilhermeMarques1 <g-marques@outlook.com>
2026-03-21 16:36:46 -0300

    feat: Add ActiveLink component for improved navigation in header

--- site-blog/src/components/header/index.tsx
+++ site-blog/src/components/header/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Button } from "../ui/button";
 import { useRouter } from "next/router";
 import { cn } from "@/lib/utils";
+import { ActiveLink } from "../active-link";
 
 export const Header = () => {
   const router = useRouter();
@@ -16,9 +17,15 @@ export const Header = () => {
           <Link href="/">Logo</Link>
 
           <nav className="flex items-center gap-6">
-            <Link href="/" className={cn('text-sm font-medium transition-colors hover:text-primary', isHomePage ? 'text-blue-500' : 'text-muted-foreground')}>Início</Link>
-            <Link href="/blog" className={cn('text-sm font-medium transition-colors hover:text-primary', isBlogPage ? 'text-blue-500' : 'text-muted-foreground')}>Blog</Link>
-            <Button variant="secondary">Começar</Button>
+            <ActiveLink href="/">
+              Início
+            </ActiveLink>
+            <ActiveLink href="/blog">
+              Blog
+            </ActiveLink>
+            <Button variant="secondary" asChild>
+              <Link href="/comecar">Começar</Link>
+            </Button>
           </nav>
         </div>
 
