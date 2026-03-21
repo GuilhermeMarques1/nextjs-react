d39177517e91ad1426ec0cf409ed4c7022a2e48f
GuilhermeMarques1 <g-marques@outlook.com>
2026-03-21 19:24:08 -0300

    feat: add HeroSection component and integrate hero SVG for enhanced visual appeal

--- site-blog/src/pages/index.tsx
+++ site-blog/src/pages/index.tsx
@@ -1,18 +1,11 @@
-import { Geist, Geist_Mono } from 'next/font/google';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import { HeroSection } from "@/components/hero-section";
 
 export default function Home() {
   return (
-    <div
-    ></div>
+    <>
+      <article className="flex flex-col">
+        <HeroSection />
+      </article>
+    </>
   );
 }
