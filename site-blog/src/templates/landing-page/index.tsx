94b0559d030c22957c60a399f2a24083b96cda05
GuilhermeMarques1 <g-marques@outlook.com>
2026-05-10 20:28:55 -0300

    fix: adjusts in layout

--- site-blog/src/templates/landing-page/index.tsx
+++ site-blog/src/templates/landing-page/index.tsx
@@ -12,7 +12,6 @@ export const LandingPage = () => {
         <FeatureSection />
         <SupportSection />
         <CustomerStorySection />
-        <CallToAction />
       </article>
     </>
   );
