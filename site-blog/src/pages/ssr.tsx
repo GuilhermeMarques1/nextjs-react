02ffaff2118f18acad2b0884cfcc413b1cf47c9f
GuilhermeMarques1 <g-marques@outlook.com>
2026-05-25 23:22:49 -0300

    feat: add ISR, SSG, and SSR pages with current time display

---
+++ site-blog/src/pages/ssr.tsx
@@ -0,0 +1,26 @@
+import { GetServerSideProps } from "next";
+
+interface SSRPageProps {
+  currentTime: string;
+}
+
+export default function SSRPage({ currentTime }: SSRPageProps) {
+  return (
+    <div className="mt-10 flex flex-col items-center text-center">
+      <h1 className="text-heading-hg text-blue-200">
+        Server Side Rendering (SSR)
+      </h1>
+      <p className="text-gray-200">
+        Current time: {currentTime}
+      </p>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<SSRPageProps> = async () => {
+  const currentTime = new Date().toISOString();
+
+  return {
+    props: { currentTime },
+  }
+}
